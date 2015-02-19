f98e77112a3c90e026e86ec7195b2f34c7510ab2
ruan <ruan9422@gmail.com>
2015-02-19 13:56:07 +0200

    Fix table_prefix support

--- includes/data/database.php
+++ includes/data/database.php
@@ -8,6 +8,7 @@ $table_prefix = "";
 $table_bans = $table_prefix . "bans";
 $table_mutes = $table_prefix . "mutes";
 $table_warnings = $table_prefix . "warnings";
+$table_history = $table_prefix . "history";
 
 if (!$conn) {
     die('Could not connect: ' . mysql_error());
