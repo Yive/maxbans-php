20e7a690031d37fda26a69732b6d56db942d9ed0
ruan <ruan9422@gmail.com>
2015-02-22 16:06:50 +0200

    Add bans, mutes and warnings to header

--- includes/header.php
+++ includes/header.php
@@ -12,15 +12,9 @@
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
                 <li><a href="index.php">Home</a></li>
-                <li><a data-toggle="modal" data-target="#about">About</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Categories <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="bans.php">Bans</a></li>
-                        <li><a href="mutes.php">Mutes</a></li>
-                        <li><a href="warnings.php">Warnings</a></li>
-                    </ul>
-                </li>
+                <li><a href="bans.php">Bans</a></li>
+                <li><a href="mutes.php">Mutes</a></li>
+                <li><a href="warnings.php">Warnings</a></li>
             </ul>
         </div>
     </div>
