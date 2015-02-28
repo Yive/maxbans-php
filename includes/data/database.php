1e0962e883b271de1ad0cf74f71f6d2f9fe655d7
ruan <ruan9422@gmail.com>
2015-02-28 17:41:38 +0200

    Update database.php

--- includes/data/database.php
+++ includes/data/database.php
@@ -1,8 +1,15 @@
 <?php
-$dbhost = 'localhost';
-$dbuser = 'root';
-$dbpass = 'password';
-$conn = new mysqli($dbhost, $dbuser, $dbpass, 'litebans');
+// Server host
+$dbhost   = 'localhost';
+
+// Username/password
+$username = 'root';
+$password = 'password';
+
+// Database name
+$database = 'litebans';
+
+$conn = new mysqli($dbhost, $username, $password, $database);
 
 $table_prefix = "";
 $table_bans = $table_prefix . "bans";
@@ -10,7 +17,7 @@ $table_mutes = $table_prefix . "mutes";
 $table_warnings = $table_prefix . "warnings";
 $table_history = $table_prefix . "history";
 
-if($conn->connect_errno > 0){
+if($conn->connect_errno > 0) {
     die('Unable to connect to database [' . $conn->connect_error . ']');
 }
 
