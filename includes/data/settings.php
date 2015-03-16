30c53a5c384723b0d557f72bdbf32ac7401facb8
Yive <admin@yive.me>
2015-03-16 15:21:06 -0700

    Removed terrible code & added statement text.
    Statement text is used to write a paragraph or whatever for what the
    page is for.

--- includes/data/settings.php
+++ includes/data/settings.php
@@ -4,4 +4,6 @@ $name = 'Maxbans';
 $serverip = 'us.mineplex.com';
 // Either "Light" or "Dark" //
 $css = "Dark";
+// Index text //
+$statement = "Here is where our Bans, IP-Bans, Mutes & Warnings are listed.";
 ?>
