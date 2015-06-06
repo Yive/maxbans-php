4cf39ef3e42add4a9b4e1e8619243a3a4e281c7b
ruan <ruan9422@gmail.com>
2015-06-06 08:59:22 +0200

    Allow driver to be changed in settings.php

--- includes/settings.php
+++ includes/settings.php
@@ -22,17 +22,18 @@ $limit_per_page = 20;
 // If you set a table prefix in config.yml, put it here too
 $table_prefix = "";
 
+$driver = 'mysql';
 
 /*****************************************************************************/
 function litebans_connect() {
     // imported
-    global $dbhost, $username, $password, $database, $table_prefix, $show_inactive_bans;
+    global $dbhost, $username, $password, $database, $table_prefix, $show_inactive_bans, $driver;
 
     // exported
     global $conn, $active_query;
     global $table_bans, $table_mutes, $table_warnings, $table_history;
 
-    $dsn = 'mysql:dbname=' . $database . ';host=' . $dbhost . ';charset=utf8';
+    $dsn = $driver . ':dbname=' . $database . ';host=' . $dbhost . ';charset=utf8';
 
     try {
         $conn = new PDO($dsn, $username, $password);
