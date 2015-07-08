c75b55f41d7f55844a0031da8b8b89e15ce55b76
ruan <ruan9422@gmail.com>
2015-07-08 09:27:51 +0200

    Remove unused code

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,6 @@
 <div class="navbar navbar-default navbar-fixed-bottom" role="navigation">
     <div class="container">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
             <p class="navbar-text" style="font-size:15px;color:white;">&copy; <a
                     href="https://www.spigotmc.org/resources/litebans.3715/" target="_blank">LiteBans</a>
         </div>
