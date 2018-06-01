cbbe91782f4c6cd000a5bb8449be9a29daa07ce5
Patbox <39821509+Patbox@users.noreply.github.com>
2018-06-01 17:27:17 +0200

    Deleting style

--- bans.php
+++ bans.php
@@ -1,70 +1,55 @@
 <?php
-   include 'includes/head.php';
-   include 'includes/header.php';
-   ?>
-<head>
-   <title>Bans/Tempbans - <?php echo $name; ?></title>
-</head>
-<?php
    // <<-----------------Database Connection------------>> //
    require 'includes/data/database.php';
    $sql = 'SELECT name, reason, banner, time, expires FROM bans ORDER BY time DESC LIMIT 20';
    $retval = $conn->query($sql);
    ?>
 <body>
-   <div class="container content">
-      <div class="row">
-         <div class="col-lg-12">
-            <h1 class="page-header">Bans</h1>
-            <table class="table table-hover table-bordered table-condensed">
-            <thead>
-               <tr>
-                  <th>
-                     <center>Name</center>
-                  </th>
-                  <th>
-                     <center>Banned By</center>
-                  </th>
-                  <th>
-                     <center>Reason</center>
-                  </th>
-                  <th>
-                     <center>Banned On</center>
-                  </th>
-                  <th>
-                     <center>Banned Until</center>
-                  </th>
-               </tr>
-            </thead>
-            <tbody>
-               <?php while($row = $retval->fetch_assoc()) { 
-                  if($row['banner'] == null) {
-                     $row['banner'] = 'Console';
-                  }
-                  // <<-----------------Ban Date Converter------------>> //
-                  $timeEpoch = $row['time'];
-                  $timeConvert = $timeEpoch / 1000;
-                  $timeResult = date('F j, Y, g:i a', $timeConvert);
-                  // <<-----------------Expiration Time Converter------------>> //
-                  $expiresEpoch = $row['expires'];
-                  $expiresConvert = $expiresEpoch / 1000;
-                  $expiresResult = date('F j, Y, g:i a', $expiresConvert);
-                  ?>
-               <tr>
-                  <td><?php echo "<img src='http://cravatar.eu/avatar/" . $row['name'] . "/25' style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['name'];?></td>
-                  <td><?php echo "<img src='http://cravatar.eu/avatar/" . $row['banner'] . "/25'  style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['banner'];?></td>
-                  <td style="width: 30%;"><?php echo $row['reason'];?></td>
-                  <td><?php echo $timeResult;?></td>
-                  <td><?php if($row['expires'] == 0) {
-                     echo 'Permanent Ban';
-                     } else {
-                     echo $expiresResult; }?></td>
-               </tr>
-               <?php }
-                  $conn->close();
-                  echo "</tbody></table>";
-                  ?>
-         </div>
-      </div>
-      <?php include 'includes/footer.php'; ?>
-   </div>
+	<table class="col-sm-12 table-condensed">
+		<thead>
+			<tr>
+				<th>
+					<center>Name</center>
+				</th>
+				<th>
+					<center>Banned By</center>
+				</th>
+				<th>
+					<center>Reason</center>
+				</th>
+				<th>
+					<center>Banned On</center>
+				</th>
+				<th>
+					<center>Banned Until</center>
+				</th>
+			</tr>
+			</thead>
+			<tbody>
+			   <?php while($row = $retval->fetch_assoc()) { 
+				  if($row['banner'] == null) {
+					 $row['banner'] = 'Console';
+				  }
+				  // <<-----------------Ban Date Converter------------>> //
+				  $timeEpoch = $row['time'];
+				  $timeConvert = $timeEpoch / 1000;
+				  $timeResult = date('F j, Y, g:i a', $timeConvert);
+				  // <<-----------------Expiration Time Converter------------>> //
+				  $expiresEpoch = $row['expires'];
+				  $expiresConvert = $expiresEpoch / 1000;
+				  $expiresResult = date('F j, Y, g:i a', $expiresConvert);
+				  ?>
+			   <tr>
+				  <td><?php echo "<img src='http://cravatar.eu/avatar/" . $row['name'] . "/25' style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['name'];?></td>
+				  <td><?php echo "<img src='http://cravatar.eu/avatar/" . $row['banner'] . "/25'  style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['banner'];?></td>
+				  <td style="width: 30%;"><center><?php echo $row['reason'];?></center></td>
+				  <td><?php echo $timeResult;?></td>
+				  <td><center><?php if($row['expires'] == 0) {
+					 echo 'Permanent Ban';
+					 } else {
+					 echo $expiresResult; }?></center></td>
+			   </tr>
+			   <?php }
+				  $conn->close();
+				  echo "</tbody></table>";
+				  ?>
