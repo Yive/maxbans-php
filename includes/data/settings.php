68b157df8a0b9ef99e63be98134b07ce6ec5dc89
Patbox <39821509+Patbox@users.noreply.github.com>
2018-06-01 17:32:47 +0200

    Delete settings.php

--- includes/data/settings.php
+++
@@ -1,9 +0,0 @@
-<?php
-// <<---------Ban Listing Settings (Simplified)--------->> //
-$name = 'Maxbans';
-$serverip = 'us.mineplex.com';
-// Either "Light" or "Dark" //
-$css = "Dark";
-// Index text //
-$statement = "Here is where our Bans, IP-Bans, Mutes & Warnings are listed.";
-?>
