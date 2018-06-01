5ebdc3db7956bb94cbfb10e4285cf79e293a0877
Patbox <39821509+Patbox@users.noreply.github.com>
2018-06-01 17:28:37 +0200

    Update mutes.php

--- mutes.php
+++ mutes.php
@@ -1,22 +1,11 @@
 <?php
-   include 'includes/head.php';
-   include 'includes/header.php';
-   ?>
-<head>
-   <title>Mutes/TempMutes - <?php echo $name; ?></title>
-</head>
-<?php
    // <<-----------------Database Connection------------>> //
    require 'includes/data/database.php';
    $sql = 'SELECT name, reason, muter, time, expires FROM mutes ORDER BY time DESC LIMIT 20';
    $retval = $conn->query($sql);
    ?>
 <body>
-   <div class="container content">
-      <div class="row">
-         <div class="col-lg-12">
-            <h1 class="page-header">Mutes</h1>
-            <table class="table table-hover table-bordered table-condensed">
+            <table class="col-sm-12 table-condensed">
             <thead>
                <tr>
                   <th>
@@ -51,20 +40,16 @@
                   $expiresResult = date('F j, Y, g:i a', $expiresConvert);
                   ?>
                <tr>
-                  <td><?php echo "<img src='http://cravatar.eu/avatar/" . $row['name'] . "/25' style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['name'];?></td>
-                  <td><?php echo "<img src='http://cravatar.eu/avatar/" . $row['banner'] . "/25'  style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['banner'];?></td>
-                  <td style="width: 30%;"><?php echo $row['reason'];?></td>
-                  <td><?php echo $timeResult;?></td>
-                  <td><?php if($row['expires'] == 0) {
-                     echo 'Permanent Mute';
-                     } else {
-                     echo $expiresResult; }?></td>
-               </tr>
-               <?php }
-                  $conn->close();
-                  echo "</tbody></table>";
-                  ?>
-         </div>
-      </div>
-      <?php include 'includes/footer.php'; ?>
-   </div>
+				  <td><?php echo "<img src='http://cravatar.eu/avatar/" . $row['name'] . "/25' style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['name'];?></td>
+				  <td><?php echo "<img src='http://cravatar.eu/avatar/" . $row['banner'] . "/25'  style='margin-bottom:5px;margin-right:5px;border-radius:2px;' />" . $row['banner'];?></td>
+				  <td style="width: 30%;"><center><?php echo $row['reason'];?></center></td>
+				  <td><?php echo $timeResult;?></td>
+				  <td><center><?php if($row['expires'] == 0) {
+					 echo 'Permanent Mute';
+					 } else {
+					 echo $expiresResult; }?></center></td>
+			   </tr>
+			   <?php }
+				  $conn->close();
+				  echo "</tbody></table>";
+				  ?>
